00854e0aeeca6b808b6a81a0b264808f5429035b
E164049P <admin>
2018-03-27 17:09:02 +0200

    commentaires

--- server.py
+++ server.py
@@ -10,74 +10,79 @@ import sys
 
 class Equipements_activites(object):
 
-
+#Retourne un tableau d'activités corresondant à la ville entrée en parametre
     def getActiviteByVille(self, ville):
         try:
-            conn = sqlite3.connect('labase.db')
+            conn = sqlite3.connect('labase.db') # Connexion à la base de donnée
             c = conn.cursor()
-            conn.text_factory = str
-            activites = c.execute("SELECT ActLib FROM 'activites' WHERE ComLib='"+ville+"'")
-            retour =  activites.fetchall()
+            conn.text_factory = str # Le type de donnée en retour de la base est string
+            activites = c.execute("SELECT ActLib FROM 'activites' WHERE ComLib='"+ville+"'") # On demande toutes les activités dans la ville demandée
+            retour =  activites.fetchall() # On réorganise les valeurs retournées dans un tableau
             conn.commit()
-            conn.close()
-            return retour
-        except sqlite3.OperationalError as e:
-            print('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville))
-            return('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville))
+            conn.close() # On termine la connexion avec la base de donnée
+            return retour # On retourne le tableau
+        except sqlite3.OperationalError as e: # Au cas où le serveur ne trouve pas la table cherchée
+            print('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville)) # On affiche l'erreur dans la console du serveur
+            return('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville)) # On retourne la meme erreur pour ensuite l'afficher au client
 
 class Equipements(object):
 
-
+#Retourne un tableau d'equipements corresondant à la ville entrée en parametre
     def getEquipementsByVille(self, ville):
         try:
-            conn = sqlite3.connect('labase.db')
+            conn = sqlite3.connect('labase.db') # Connexion à la base de donnée
             c = conn.cursor()
-            conn.text_factory = str
-            activites = c.execute("SELECT EquNom FROM 'equipements' WHERE ComLib='"+ville+"'")
+            conn.text_factory = str # Le type de donnée en retour de la base est string
+            activites = c.execute("SELECT EquNom FROM 'equipements' WHERE ComLib='"+ville+"'") # On demande toutes les activités dans la ville demandée
             conn.commit()
-            retour = activites.fetchall()
-            conn.close()
-            return retour
+            retour = activites.fetchall() # On réorganise les valeurs retournées dans un tableau
+            conn.close() # On termine la connexion avec la base de donnée
+            return retour # On retourne le tableau
         except sqlite3.OperationalError as e:
-            print('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville))
-            return('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville))
+            print('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville)) # On affiche l'erreur dans la console du serveur
+            return('[-] Sqlite operational error: {}, ville: {}, Abandon'.format(e, ville)) # On retourne la meme erreur pour ensuite l'afficher au client
 
 _allow_origin = '*'
 _allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
 _allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
 
+# Permet de choisir les headers de les reponses emisent par le serveur
 @hook('after_request')
 def enable_cors():
     '''Add headers to enable CORS'''
-
+#Access-Control-Allow-Origin nous permet de pouvoir utiliser l'API qui est sur localhost alors que nous sommes sur un autre domaine
     response.headers['Access-Control-Allow-Origin'] = _allow_origin
+#Access-Control-Allow-Methods permet de choisir quelle methode du protocole HTTP sont utilisables
     response.headers['Access-Control-Allow-Methods'] = _allow_methods
+#Access-Control-Allow-Headers permet de choisir les headers modifiables
     response.headers['Access-Control-Allow-Headers'] = _allow_headers
 
+# Si le client demande les activités d'une ville
 @get('/api/activites/<ville>')
 def index(ville):
-    activites = Equipements_activites()
-    liste = activites.getActiviteByVille(ville)
-    response.headers['Content-Type'] = 'application/json'
+    activites = Equipements_activites() # Nouvel obet equipement
+    liste = activites.getActiviteByVille(ville) # On execute la requete sur la base de donnée
+    response.headers['Content-Type'] = 'application/json' # On defini que le type de retour sera du Json
     reussite = "true"
-    if (type(liste) is str):
-        reussite = "false"
-        return { "reussite" : reussite, "message" : liste}
-    return { "reussite" : reussite, "activites" : liste}
+    if (type(liste) is str): # Si le resultat de la requete sur la base est un message
+        reussite = "false" # C'est une message d'erreur donc reussite passe à false
+        return { "reussite" : reussite, "message" : liste} # On renvoie une reussite false et le message d'erreur au client
+    return { "reussite" : reussite, "activites" : liste} # Si tout c'est bien passé on renvoie une reussite true et la table
 
+# Si le client demande les équipements d'une ville
 @get('/api/equipements/<ville>')
 def index(ville):
-    equipements = Equipements()
-    liste = equipements.getEquipementsByVille(ville)
-    response.headers['Content-Type'] = 'application/json'
+    equipements = Equipements() # Nouvel obet equipement
+    liste = equipements.getEquipementsByVille(ville) # On execute la requete sur la base de donnée
+    response.headers['Content-Type'] = 'application/json' # On defini que le type de retour sera du Json
     reussite = "true"
-    if (type(liste) is str):
-        reussite = "false"
-        return { "reussite" : reussite, "message" : liste}
-    return { "reussite" : reussite, "equipements" : liste}
+    if (type(liste) is str): # Si le resultat de la requete sur la base est un message
+        reussite = "false" # C'est une message d'erreur donc reussite passe à false
+        return { "reussite" : reussite, "message" : liste} # On renvoie une reussite false et le message d'erreur au client
+    return { "reussite" : reussite, "equipements" : liste} # Si tout c'est bien passé on renvoie une reussite true et la table
 
 
 
 if __name__ == '__main__':
 
-    run(host='localhost', port=2056)
+    run(host='localhost', port=2056) # Le serveur écoute sur localhost:2056
--- vue/script/main.js
+++ vue/script/main.js
@@ -3,31 +3,31 @@ $(window).on('load', function () {
 //Autocomplete
   var tags = [];
   $( "#inputNomDeVille" ).autocomplete({
-    source: function (request, response) {
+    source: function (request, response) { // La liste de villes proposées se met à jour dès que 3 caractères sont rentrés
       $.get("http://infoweb-ens/~jacquin-c/codePostal/commune.php?commune="+$("#inputNomDeVille").val()+"&maxRows=3", function (reponse) {
+        // Requete contenant les lettres entrées dans l'input et maxRow étant le nombre de ville à renvoyer
         let i = 0
         let sourceArray = [];
-        for (ville of reponse) {
-          sourceArray[i] = {label:ville.Ville, value:ville.Ville}
+        for (ville of reponse) { // Création du dictionnaire servant de liste de proposition à jquery
+          sourceArray[i] = {label:ville.Ville, value:ville.Ville} // label = texte affiché // Value = texte envoyé
           i++
         }
         response(sourceArray)
       })
     },
-    minLength: 3
+    minLength: 3 // Nombre de lettres necessaires à l'affichage de propositions
   });
 
+//Recherche en fonction de la ville et des valeurs cochées/rentrées
   $("#recherche").on("click", function(event) {
     event.preventDefault()
-    console.log($("#type").val())
-      console.log($("#inputNomDeVille").val())
-    if ($("#type").val() != "" && $("#inputNomDeVille").val() != "") {
-      console.log("localhost:2056/api/"+$("#type").val()+"/"+$("#inputNomDeVille").val())
+    if ($("#type").val() != "" && $("#inputNomDeVille").val() != "") { // Si l'utilisateur a rentré une ville
       $.get("http://localhost:2056/api/"+$("#type").val()+"/"+$("#inputNomDeVille").val(), function(response) {
         //http://localhost:2056/api/activites/Vertou
-        $('#resultat').empty()
-        if (response.reussite == "true") {
-          if (response[$("#type").val()].length > 0) {
+        // Réquete sur l'api bottle (notre serveur python) contenant le type de requete (activites ou equipements) et la ville concernée
+        $('#resultat').empty() // On vide la partie de la page reservée à l'affichage
+        if (response.reussite == "true") { // Si le message renvoyé est positif
+          if (response[$("#type").val()].length > 0) { // Si le tableau n'est pas vide
           $('#resultat').append(`
             <thead>
               <tr>
@@ -36,10 +36,9 @@ $(window).on('load', function () {
               </tr>
             </thead>
             <tbody>
-            </tbody>`)
+            </tbody>`) // On ajoute l'entete du tableau
           let i = 0
-          for (element of response[$("#type").val()]) {
-            console.log(element)
+          for (element of response[$("#type").val()]) { // On ajoute toute les valeurs dans tableau
             $("#resultat tbody").append(
               `
               <tr>
@@ -50,16 +49,16 @@ $(window).on('load', function () {
             )
             i++
           }
-          $('#resultat').DataTable();
-        } else {
-          $("#resultat").append("<p>Pas de donnée sur cette ville</p>")
+          $('#resultat').DataTable(); // On active DataTable de jQuery ui sur le tableau
+        } else { // Si le tableau recu est vide
+          $("#resultat").append("<p>Pas de donnée sur cette ville</p>") // On l'affiche au client
         }
-      } else {
-        alert("Erreur lors de traitement de la requete:\n" + response.message)
+      } else { // Si reussiter == false
+        alert("Erreur lors de traitement de la requete:\n" + response.message) // On affiche un pop-up avec le message d'erreur du serveur
       }
       })
-      .fail(function(response) {
-  console.log( response );
+      .fail(function(response) { // En cas d'erreur de communication avec le serveur on l'affiche au client
+        alert("Erreur de communication avec le serveur");
 })
     }
   })
